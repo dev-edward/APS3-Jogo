9639b21289e9089b99dec83cd1696be33dc6295c
edward-d3v <42708123+edward-d3v@users.noreply.github.com>
2024-05-01 02:04:59 -0300

    Adicionando carro e casa para teste

---
+++ src/jogo/Modelo/Casa.java
@@ -0,0 +1,35 @@
+package jogo.Modelo;
+import javax.swing.*;
+import java.awt.*;
+
+public class Casa {
+    private Image imagem;
+    private int x,y;
+    private int largura,altura;
+
+    public Casa(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void load(){
+        ImageIcon referencia = new ImageIcon("src/res/casa.png");
+        imagem = referencia.getImage();
+        this.largura = imagem.getWidth(null);
+        this.altura = imagem.getHeight(null);
+    }
+    public void update(){
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImagem() {
+        return imagem;
+    }
+}
--- src/jogo/Modelo/Fase.java
+++ src/jogo/Modelo/Fase.java
@@ -2,18 +2,64 @@ package jogo.Modelo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-public class Fase extends JPanel {
+public class Fase extends JPanel implements ActionListener {
     private Image fundo;
+    private Player player;
+    private Casa casa;
+    private Timer timer;
+
+
 
     public Fase() {
-        ImageIcon referencia = new ImageIcon("src/res/fundo.jpeg");
+        setFocusable(true);
+        setDoubleBuffered(true);
+        ImageIcon referencia = new ImageIcon("src/res/fundo.png");
         fundo = referencia.getImage();
+        player = new Player();
+        player.load();
+        addKeyListener(new TecladoAdapter());
+        timer = new Timer(5, this);
+        timer.start();
+        inicializarCasas();
+    }
+    public void inicializarCasas() {
+        //int coordenadas[] = new int[4];
+        //casa = new ArrayList<Casa>();
+        casa = new Casa(200,200);
+        casa.load();
+
     }
     public void paint(Graphics g) {
+        super.paint(g);
         Graphics2D graficos = (Graphics2D) g;
         graficos.drawImage(fundo, 0, 0, null);
+        graficos.drawImage(player.getImagem(),player.getX(),player.getY(),this);
+        graficos.drawImage(casa.getImagem(),casa.getX(),casa.getY(),this);
+
         g.dispose();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        player.update();
+        repaint();
+    }
+    private class TecladoAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            player.keyPressed(e);
+        }
+        @Override
+        public void keyReleased(KeyEvent e) {
+            player.keyRelease(e);
+        }
     }
 }
 
